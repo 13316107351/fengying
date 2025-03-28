2e152b433f1101a485a98543690a2d8269ae22fd
fengying <13316107351@163.com>
2025-03-28 15:07:48 +0800

    Auto update: 2025/03/28 周五 15:07:48.89

--- Build/tianwen1-final.loader.js
+++ Build/tianwen1-final.loader.js
@@ -1,1 +1,38 @@
-// loader content placeholder
+function createUnityInstance(canvas, config, onProgress) {
+  var script = document.createElement("script");
+  script.src = config.frameworkUrl;
+  script.onload = function () {
+    var unityModule = {
+      preRun: [],
+      postRun: [],
+      print: function (text) {
+        console.log(text);
+      },
+      canvas: canvas,
+      locateFile: function (url) {
+        return url;
+      }
+    };
+
+    canvas.className = "unity-canvas";
+    var loadingInfo = document.createElement("div");
+    loadingInfo.innerText = "Loading Unity WebGL...";
+    canvas.parentNode.insertBefore(loadingInfo, canvas.nextSibling);
+
+    var scriptWasm = document.createElement("script");
+    scriptWasm.src = config.codeUrl;
+    scriptWasm.onload = function () {
+      loadingInfo.innerText = "WASM loaded.";
+      if (typeof onProgress === "function") {
+        onProgress({ progress: 1.0 });
+      }
+      console.log("Unity instance simulated (framework/wasm loaded).");
+    };
+    document.body.appendChild(scriptWasm);
+  };
+  document.body.appendChild(script);
+
+  return new Promise(function (resolve) {
+    resolve({ SendMessage: function () {}, Quit: function () {} });
+  });
+}
